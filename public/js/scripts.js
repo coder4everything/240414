36def3538a5c76cf2038bfda306c2d3a69388a32
yujeong <yujung917@daum.net>
2024-04-15 23:36:38 +0900

    위치 함수 프로미스로 변경

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,9 +6,30 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+//navigator.geolocation.getCurrentPosition정보참조
+//https://velog.io/@hayeooooon/2022-08-06
+function getCurrentPosition() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                resolve(position);
+            },
+            error => {
+                reject(error);
+            }
+        );
+    });
+}
+
 function setLocation(){
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-  });
+  // 프로미스를 사용하여 위치 정보 가져오기
+  // 그냥 프로미스 써보고 싶었음
+  getCurrentPosition()
+      .then(position => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+      })
+      .catch(error => {
+          console.error('위치 정보를 가져오는 동안 오류 발생:', error);
+      });
 }
