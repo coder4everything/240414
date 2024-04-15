beae02d4888cf7bbda14b56fe9542ac99f276eb0
yujeong <yujung917@daum.net>
2024-04-15 17:18:08 +0900

    라우터 설정

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
+const express = require('express');
+
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+const unloginedRouter = require('./routes/unlogined');
 
 const app = express();
 app.set('port',process.env.PORT || 5000);
 
-app.get('/',(req,res)=>{
-  res.send('Hello,Express');
-})
+app.use('/',indexRouter);
+app.use('/unlogined',unloginedRouter);
+app.use('/user',userRouter);
+
 
 app.listen(app.get('port'), ()=>{
   console.log(app.get('port'),'번 포트에서 대기중');
